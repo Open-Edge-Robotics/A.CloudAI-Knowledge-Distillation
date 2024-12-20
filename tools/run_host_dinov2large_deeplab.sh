f35f5d14338f49ab7c6f3c170e5b119b9752cbf4
birdomi <ryk012@naver.com>
2024-12-20 10:34:10 +0900

    Fix: Remove redundant code

--- tools/run_host_dinov2large_deeplab.sh
+++
@@ -1,6 +0,0 @@
-python tools/run_host_full.py \
-    configs/temp/deeplabv3plus_r18-d8_4xb2-80k_cityscapes-512x1024_acdc.py \
-    ./deeplabv3plus_r18-d8_512x1024_80k_cityscapes_20201226_080942-cff257fe.pth \
-    --config_t configs/dinov2_citys2acdc/rein_dinov2l_mask2former_1024x1024_bs4x2.py \
-    --checkpoint_t ./work_dirs/rein_dinov2l_mask2former_1024x1024_bs4x2/iter_40000.pth \
-    --backbone ./checkpoints/dinov2l_converted_1024x1024.pth
--- tools/run_self_dino_kd_n.sh
+++
@@ -1,6 +0,0 @@
-python tools/run_client_self_kd_n.py \
-    configs/temp/segformer_mit-b1_acdc.py \
-    ./segformer_mit-b1_8x1_1024x1024_160k_cityscapes_20211208_064213-655c7b3f.pth \
-    --config_t configs/dinov2_citys2acdc/rein_dinov2s_mask2former_1024x1024_bs4x2.py \
-    --checkpoint_t ./work_dirs/rein_dinov2s_mask2former_1024x1024_bs4x2/iter_40000.pth \
-    --backbone ./checkpoints/dinov2s_converted_1024x1024.pth
