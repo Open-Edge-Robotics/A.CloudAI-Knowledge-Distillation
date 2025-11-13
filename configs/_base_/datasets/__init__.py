80727a6b25abb0fe10c202dfbed4897efcaace66
Sangboom <saturnbum@naver.com>
2025-11-13 17:17:03 +0900

    feat: make segformer config shift dataset

--- configs/_base_/datasets/__init__.py
+++ configs/_base_/datasets/__init__.py
@@ -1,30 +1,30 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # yapf: disable
-from .ade import ADE20KDataset
-from .basesegdataset import BaseCDDataset, BaseSegDataset
-from .bdd100k import BDD100KDataset
-from .chase_db1 import ChaseDB1Dataset
+# from .ade import ADE20KDataset
+# from .basesegdataset import BaseCDDataset, BaseSegDataset
+# from .bdd100k import BDD100KDataset
+# from .chase_db1 import ChaseDB1Dataset
 from .cityscapes import CityscapesDataset
-from .coco_stuff import COCOStuffDataset
-from .dark_zurich import DarkZurichDataset
-from .dataset_wrappers import MultiImageMixDataset
-from .decathlon import DecathlonDataset
-from .drive import DRIVEDataset
-from .dsdl import DSDLSegDataset
-from .hrf import HRFDataset
-from .isaid import iSAIDDataset
-from .isprs import ISPRSDataset
-from .levir import LEVIRCDDataset
-from .lip import LIPDataset
-from .loveda import LoveDADataset
-from .mapillary import MapillaryDataset_v1, MapillaryDataset_v2
-from .night_driving import NightDrivingDataset
-from .nyu import NYUDataset
-from .pascal_context import PascalContextDataset, PascalContextDataset59
-from .potsdam import PotsdamDataset
-from .refuge import REFUGEDataset
-from .stare import STAREDataset
-from .synapse import SynapseDataset
+# from .coco_stuff import COCOStuffDataset
+# from .dark_zurich import DarkZurichDataset
+# from .dataset_wrappers import MultiImageMixDataset
+# from .decathlon import DecathlonDataset
+# from .drive import DRIVEDataset
+# from .dsdl import DSDLSegDataset
+# from .hrf import HRFDataset
+# from .isaid import iSAIDDataset
+# from .isprs import ISPRSDataset
+# from .levir import LEVIRCDDataset
+# from .lip import LIPDataset
+# from .loveda import LoveDADataset
+# from .mapillary import MapillaryDataset_v1, MapillaryDataset_v2
+# from .night_driving import NightDrivingDataset
+# from .nyu import NYUDataset
+# from .pascal_context import PascalContextDataset, PascalContextDataset59
+# from .potsdam import PotsdamDataset
+# from .refuge import REFUGEDataset
+# from .stare import STAREDataset
+# from .synapse import SynapseDataset
 from .acdc import ACDCDataset
 from .shift import ShiftDataset
 # yapf: disable
