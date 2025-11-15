d1aab917f1e85240e4c48ad69d8970cd88e8232f
Sangboom <saturnbum@naver.com>
2025-11-15 21:05:31 +0900

    exp: make bash file for experiment

---
+++ tools/run_client_deeplabv3.sh
@@ -0,0 +1,4 @@
+python tools/run_client.py \
+    configs/temp/deeplabv3plus_r18-d8_4xb2-80k_cityscapes-512x1024_acdc.py \
+    ./deeplabv3plus_r18-d8_512x1024_80k_cityscapes_20201226_080942-cff257fe.pth \
+    --save_path internImageMask2Former-2-DeepLabv3_plus_r18-AlwaysDistill-ACDC100
