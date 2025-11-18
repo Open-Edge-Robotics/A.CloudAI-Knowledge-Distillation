b786749458389972750dba15b6ce415ef60383e9
Sangboom <saturnbum@naver.com>
2025-11-18 18:03:21 +0900

    debug: fix run_client_sebg1_shift empty

---
+++ tools/run_client_segb1_shift.sh
@@ -0,0 +1,4 @@
+python tools/run_client.py \
+    configs/segformer/segformer_mit-b1_8xb1-160k_cityscapes-1024x1024_shift.py \
+    ./work_dirs/segformer_mit-b1_8xb1-160k_cityscapes-1024x1024_shift/iter_160000.pth \
+    --save_path SHIFT_Ours
