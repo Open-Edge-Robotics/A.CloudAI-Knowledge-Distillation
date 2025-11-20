7f3f4b103eada16e02f15aca275868c6922ff495
Sangboom <saturnbum@naver.com>
2025-11-20 10:27:26 +0900

    exp: add run ours bash file

---
+++ tools/run_ours.sh
@@ -0,0 +1,4 @@
+python tools/run_client_ours.py \
+    configs/temp/segformer_mit-b1_acdc.py \
+    ./segformer_mit-b1_8x1_1024x1024_160k_cityscapes_20211208_064213-655c7b3f.pth \
+    --save_path Baseline
